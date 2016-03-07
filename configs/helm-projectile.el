3e121f1a754d2509708ab29cb6383fb93f57f0cd
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-03-07 20:06:52 +0100

    Use snapshot melpa

--- configs/helm-projectile.el
+++ configs/helm-projectile.el
@@ -1,4 +1,6 @@
 ;; Helm projectile
 
+(require 'helm-projectile)
+
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
--- init.el
+++ init.el
@@ -3,9 +3,15 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; load package archives if no packages installed
+
+(let ((elpa-dir (concat (file-name-as-directory user-emacs-directory) "elpa/")))
+  (when (not (file-exists-p elpa-dir))
+    (package-refresh-contents)))
 
 (package-initialize)
 
@@ -57,3 +63,17 @@
 ;; Magit
 
 (global-set-key (kbd "C-x g") 'magit-status)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (helm-projectile projectile magit undo-tree smartparens cider whole-line-or-region ace-jump-mode multiple-cursors helm company color-theme-wombat))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
