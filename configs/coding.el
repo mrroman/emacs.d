613a9c6cdae2b233e0670076fbd6b4c6d8ba75d0
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-03-08 12:00:38 +0100

    Enable whitespace-mode and turn off tabs

--- configs/coding.el
+++ configs/coding.el
@@ -3,4 +3,12 @@
 (global-company-mode t)
 (setq company-idle-delay 0.1)
 
+;; eldoc (documentation of current function)
+
 (eldoc-mode)
+
+;; don't use tabs
+
+(require 'whitespace)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
