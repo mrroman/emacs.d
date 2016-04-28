0d2c564114ce5276201acb20533c50e62b2d1e35
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-04-28 10:06:54 +0200

    Close mingus buffer.

--- configs/mingus.el
+++ configs/mingus.el
@@ -3,9 +3,12 @@
 (defun my/mingus-play-toggle ()
   "Toggle playing MPD. If MPD was playing it pauses. If MPD was paused it plays. If MPD was stopeed it starts playing."
   (interactive)
-  (case (mingus-get-a-state 'state)
-    (play (mingus-pause))
-    (pause (mingus-pause))
-    (stop (mingus-play 0))))
+  (progn
+    (mingus)
+    (case (mingus-get-a-state 'state)
+      (play (mingus-pause))
+      (pause (mingus-pause))
+      (stop (mingus-play 0)))
+    (kill-buffer)))
 
 (global-set-key [f5] 'my/mingus-play-toggle)
