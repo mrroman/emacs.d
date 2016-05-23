a9d58a3d9964a0f447872596d24dbd23f84778a9
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-05-23 14:33:48 +0200

    Terminal opens below in small window

--- configs/keybindings.el
+++ configs/keybindings.el
@@ -12,9 +12,31 @@
 
 ;; Terminal
 
+(defadvice term-handle-exit
+    (after term-kill-buffer-on-exit activate)
+  (kill-buffer-and-window))
+
+(defvar my/term-number 0)
+
+(require 'term)
+
 (global-set-key (kbd "C-c t") (lambda ()
                                 (interactive)
-                                (ansi-term (getenv "SHELL"))))
+                                (let* ((window (split-window-below -10))
+                                       (buf-name (format "ansi-term<%d>" (incf my/term-number)))
+                                       (buffer (term-ansi-make-term buf-name (getenv "SHELL"))))
+                                  (progn
+                                    (set-buffer buffer)
+                                    (term-mode)
+                                    (term-char-mode)
+                                    (whitespace-mode 0)
+                                    (let (term-escape-char)
+                                      ;; I wanna have find-file on C-x C-f -mm
+                                      ;; your mileage may definitely vary, maybe it's better to put this in your
+                                      ;; .emacs ...
+                                      (term-set-escape-char ?\C-x))
+                                    (set-window-buffer window buffer)
+                                    (select-window window)))))
 
 ;; Turn of annoying keyscores
 
