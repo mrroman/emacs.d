0f201222936d21fc46d309987b1c73c5e5e47dd3
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-08-22 09:31:39 +0200

    Set 2 space indent in package.json

--- configs/js.el
+++ configs/js.el
@@ -2,3 +2,12 @@
 
 (setq-default js2-global-externs '("module" "require" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "it" "describe" "beforeEach" "afterEach" "before" "after"))
 
+;; custom indent for package.json
+
+(defun my/custom-js-indent ()
+  (lexical-let* ((buffer-file-path (buffer-file-name (current-buffer)))
+                 (fname (file-name-nondirectory buffer-file-path)))
+                             (when (string= fname "package.json")
+                               (setq-local js-indent-level 2))))
+
+(add-hook 'js-mode-hook 'my/custom-js-indent)
