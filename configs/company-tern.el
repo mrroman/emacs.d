53269431d3da9014747144b3ed79434a95aeea72
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-09-17 09:56:27 +0300

    Enable correct highlight on .tern-project

--- configs/company-tern.el
+++ configs/company-tern.el
@@ -2,3 +2,5 @@
                            (progn
                              (add-to-list 'company-backends 'company-tern)
                              (tern-mode))))
+
+(add-to-list 'auto-mode-alist '(".tern-project" . js-mode))
