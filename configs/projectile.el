72bb94b9611d3d6fd34c3aeb387c210ea0908388
Konrad Mrozek <konrad.mrozek@gmail.com>
2016-09-22 11:44:18 +0300

    Compilation/test key binding Ctrl-F9/F9

--- configs/projectile.el
+++ configs/projectile.el
@@ -4,3 +4,6 @@
 
 (when (equalp 'windows-nt system-type)
   (setq projectile-indexing-method 'alien))
+
+(global-set-key (kbd "<f9>") 'projectile-test-project)
+(global-set-key (kbd "<C-f9>") 'projectile-compile-project)
