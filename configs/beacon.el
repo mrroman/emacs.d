faa124c60536d067050043f12254e2d299dad3e3
Konrad Mrożek <konrad.mrozek@gmail.com>
2016-10-18 22:36:53 +0200

    Remove old configuration

--- configs/beacon.el
+++
@@ -1,8 +0,0 @@
-;;; beacon --- Enable beacon-mode -*- lexical-binding: t -*-
-;;;
-;;; Commentary:
-;;; Code:
-
-(beacon-mode t)
-
-;;; beacon.el ends here
--- configs/flycheck-clojure.el
+++
@@ -1,9 +0,0 @@
-;;; flycheck-clojure --- Enable flycheck-clojure -*- lexical-binding: t -*-
-;;;
-;;; Commentary:
-;;; Code:
-
-(eval-after-load 'flycheck '(flycheck-clojure-setup))
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;;; flycheck-clojure.el ends here
